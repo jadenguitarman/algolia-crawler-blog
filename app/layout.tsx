b61bb64f45a780f6176d9dd06bf0c68c49cdd065
Jaden Baptista <jadenguitarman@gmail.com>
2024-10-21 16:56:00 -0400

    add meta and articles

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,9 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <head>
+        <meta name="algolia-site-verification"  content="6904DA043B83FDD7" />
+      </head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
