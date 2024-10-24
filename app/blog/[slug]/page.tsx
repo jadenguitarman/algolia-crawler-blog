92cf3294aa3348c50d2ed1ff3a3ebf871d0c8f75
Jaden Baptista <jadenguitarman@gmail.com>
2024-10-24 15:59:37 -0400

    finish images

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -85,6 +85,7 @@ export default function Blog({ params }) {
       <h1 className="title font-semibold text-2xl tracking-tighter">
         {post.metadata.title}
       </h1>
+      { post.metadata.image ? <img src={post.metadata.image} /> : <></> }
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
           {formatDate(post.metadata.publishedAt)}
